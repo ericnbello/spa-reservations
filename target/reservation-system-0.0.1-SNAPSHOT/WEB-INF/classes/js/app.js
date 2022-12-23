c19c00db30b5f09dab05fd47fa438cff04dbc949
Eric Bello <ebello219@gmail.com>
2022-12-22 19:29:13 -0500

    updated deploy commands

---
+++ target/reservation-system-0.0.1-SNAPSHOT/WEB-INF/classes/js/app.js
@@ -0,0 +1,15 @@
+import 'css/app.css';
+
+//function toggleNav() {
+//  var navbarToggleButton = document.getElementById('navbarToggleButton'),
+//
+//  navbarToggleButton.addEventListener("click", function() {
+//    const navToggle = document.getElementById("navbarMenu");
+//    if (navToggle.style.display === "hidden") {
+//            navToggle.classList.toggle = "block";
+//        } else {
+//            navToggle.classList.toggle = "hidden";
+//        }
+//    }
+//  }
+//}
